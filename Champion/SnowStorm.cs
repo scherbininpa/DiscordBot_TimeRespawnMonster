402124a1ec357ed0f1c2dad9407eff2d8043ebca
pa.shcherbinin <shcherbininpa@gmail.com>
2022-09-02 09:40:47 +0500

    add class SnowStorm

---
+++ Champion/SnowStorm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot_TimeRespawnMonster.Champion
+{
+    public class SnowStorm:IChampions
+    {
+        public string GetDescription() => "Снежный шторм";
+
+        public string GetName() => "SnowStorm";
+
+        public string GetPathImage() => @"imageChampions\noImage.png";
+
+        public TimeSpan MaxTimeRespawn() => DateTime.Parse("01:00").TimeOfDay;
+
+        public TimeSpan MinTimeRespawn() => DateTime.Parse("05:00").TimeOfDay;
+    }
+}
