113823e8116ce8726ab52f16b38b61f7745f1fe3
pa.shcherbinin <shcherbininpa@gmail.com>
2022-09-02 09:45:22 +0500

    add class Telek

---
+++ Champion/Telek.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot_TimeRespawnMonster.Champion
+{
+    public class Telek:IChampions
+    {
+        public string GetDescription() => "Телек";
+
+        public string GetName() => "Telek";
+
+        public string GetPathImage() => @"imageChampions\noImage.png";
+
+        public TimeSpan MaxTimeRespawn() => DateTime.Parse("00:00").TimeOfDay;
+
+        public TimeSpan MinTimeRespawn() => DateTime.Parse("01:00").TimeOfDay;
+    }
+}
