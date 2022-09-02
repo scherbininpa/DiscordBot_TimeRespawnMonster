c1722e2ca124fdbc0841ed59be9de694e2a759a1
pa.shcherbinin <shcherbininpa@gmail.com>
2022-09-02 09:38:44 +0500

    add new class MadHarpy

---
+++ Champion/MadHarpy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot_TimeRespawnMonster.Champion
+{
+    internal class MadHarpy : IChampions
+    {
+        public string GetDescription() => "Безумная гарпия";
+
+        public string GetName() => "MadHarpy";
+
+        public string GetPathImage() => @"imageChampions\noImage.png";
+
+        public TimeSpan MaxTimeRespawn() => DateTime.Parse("01:00").TimeOfDay;
+
+        public TimeSpan MinTimeRespawn() => DateTime.Parse("07:00").TimeOfDay;
+    }
+}
