352a24329474c27b223a321b6b1766be391d014a
pa.shcherbinin <shcherbininpa@gmail.com>
2022-09-02 09:42:20 +0500

    add class Rizurul

---
+++ Champion/Rizurul.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot_TimeRespawnMonster.Champion
+{
+    public class Rizurul:IChampions
+    {
+        public string GetDescription() => "Ризурул";
+
+        public string GetName() => "Rizurul";
+
+        public string GetPathImage() => @"imageChampions\noImage.png";
+
+        public TimeSpan MaxTimeRespawn() => DateTime.Parse("00:05").TimeOfDay;
+
+        public TimeSpan MinTimeRespawn() => DateTime.Parse("00:40").TimeOfDay;
+    }
+}
